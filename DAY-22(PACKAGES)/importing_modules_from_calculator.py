e5b80109500b6f121386c4da43c38b5a02e60b3d
Gantedi Srinidhi <gantedi.srinidhi@gmail.com>
2026-03-08 00:45:34 +0530

    Add files via upload

---
+++ DAY-22(PACKAGES)/importing_modules_from_calculator.py
@@ -0,0 +1,15 @@
+#from package_name.file_name import function_name
+
+from CALCULATOR.add import add
+from CALCULATOR.sub import sub
+from CALCULATOR.mul import mul
+from CALCULATOR.div import div
+
+print(add(4,5))
+print(sub(4,5))
+print(mul(4,5))
+print(div(4,5))
+
+#calculator is the package ,consisting of the varies modules
+#module is the collection of functions or classes or variables
+#function is a reusable block of code 
---
+++ DAY-22(PACKAGES)/syntax-2_for_easy_importing.py
@@ -0,0 +1,16 @@
+'''
+from CALCULATOR import add
+from CALCULATOR import sub
+from CALCULATOR import mul
+from CALCULATOR import div
+'''
+
+#Or
+#you can also write
+
+from CALCULATOR import add,sub,mul,div
+
+print(add(4,5))
+print(sub(4,5))
+print(mul(4,5))
+print(div(4,5))

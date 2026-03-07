392d47c5a4cb4b25728ee38e8adb4d79f623d739
Gantedi Srinidhi <gantedi.srinidhi@gmail.com>
2026-03-08 00:40:02 +0530

    Add files via upload

---
+++ DAY-9/quiz.py
@@ -0,0 +1,26 @@
+print(f"Question 1: What is Python? \n A) A hardware device\n B) An operating system\n C) A programming language\n D) A database")
+
+
+answer = input("Enter your choice: ")
+
+if answer == "c" or answer == "C":
+    
+  print("Correct!")
+else:
+
+  print("Incorrect")
+
+P  
+      
+print(f"Question 2: Which symbol is used to write comments in python? \n A) // \n B) # \n C) /**/ \n D)\\ ")
+
+
+answer = input("Enter your choice: ")
+
+if answer == "b" or answer == "B":
+    
+  print("Correct!")
+else:
+
+  print("Incorrect")   
+       
